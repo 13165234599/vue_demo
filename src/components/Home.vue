16194a60d2b24ac3badfbf6ddd8b298d96db0d42
甜点 <13165234599@163.com>
2020-10-20 21:48:30 +0800

    提交Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,127 @@
+<template>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <h1>甜点</h1>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" />
+        <span>电商管理后台系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 内容区域 -->
+    <el-container>
+      <!-- 侧边 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false"  router :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id +''" v-for="item in menuList" :key="item.id" unique-opened>
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index=" '/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+              <i class="el-icon-menu"></i>
+              <span>{{ subItem.authName }}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuList: [],
+      iconsObj:{
+        '125': 'iconfont icon-morentouxiang',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju-tianchong',
+        '145': 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      activePath:''
+    }
+  },
+  created() {
+    console.log('aaa');
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem('activePath')  
+  },
+  methods: {
+    logout() {
+      sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status != 200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data;
+      // console.log(res);
+    },
+    toggleCollapse(){
+      this.isCollapse = !this.isCollapse
+    },
+    saveNavState(activePath){
+      window.sessionStorage.setItem('activePath',activePath)
+
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 15px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  color: #ffffff;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
