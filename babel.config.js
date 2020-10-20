9916e5e33681978d7a2972075a6f64348f962909
甜点 <13165234599@163.com>
2020-10-20 20:42:15 +0800

    修改代码

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+//这是项目开发阶段需要用到的babel插件
+const prodPlugins = []
+if(process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +15,10 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    //发布产品时候的插件数组
+    ...prodPlugins,
+    //在 babel.config.js 配置文件中声明该插件
+    "@babel/plugin-syntax-dynamic-import"
   ]
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/users/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from  '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import Lists from '../components/goods/Lists.vue'
-import Add  from '../components/goods/Add.vue'
-import Orders from '../components/order/Order.vue'
-import Reports from '../components/report/Reports.vue'
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+// import Users from '../components/users/Users.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Roles.vue'
+// import Cate from  '../components/goods/Cate.vue'
+// import Params from '../components/goods/Params.vue'
+// import Lists from '../components/goods/Lists.vue'
+// import Add  from '../components/goods/Add.vue'
+// import Orders from '../components/order/Order.vue'
+// import Reports from '../components/report/Reports.vue'
+
+
+
+// import Login from '../components/Login.vue'
+const Login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
+// import Home from '../components/Home.vue'
+const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+// import Welcome from '../components/Welcome.vue'
+const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
+
+// import Users from '../components/user/Users.vue'
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/users/Users.vue')
+// import Rights from '../components/power/Rights.vue'
+const Rights = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+// import Roles from '../components/power/Roles.vue'
+const Roles = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+// import Cate from '../components/goods/Cate.vue'
+const Cate = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Cate.vue')
+// import Params from '../components/goods/Params.vue'
+const Params = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Params.vue')
+
+// import Lists from '../components/goods/Lists.vue'
+const Lists = () => import(/* webpackChunkName: "Lists_Add" */ '../components/goods/Lists.vue')
+// import Add from '../components/goods/Add.vue'
+const Add = () => import(/* webpackChunkName: "Lists_Add" */ '../components/goods/Add.vue')
+
+// import Orders from '../components/order/Order.vue'
+const Orders = () => import(/* webpackChunkName: "Orders_Report" */ '../components/order/Order.vue')
+// import Report from '../components/report/Report.vue'
+const Reports = () => import(/* webpackChunkName: "Orders_Report" */ '../components/report/Reports.vue')
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -43,7 +75,7 @@ router.beforeEach((to, from, next) => {
   // ${//to and from are Route Object,next() must be called to resolve the hook}
   if(to.path === '/login') return next()
   const tokenStr = sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  // if(!tokenStr) return next('/login')
   next()  
 })
 
